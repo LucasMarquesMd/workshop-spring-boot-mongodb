23adcd931b7a0a2c4a46bf6e340ae339d8864d58
Lucas Marques Mendes <lucasmqmendes@gmail.com>
2025-01-28 21:44:37 -0300

    COnsulta simples com query methods

--- src/main/java/com/lucasmd/workshopmongo/services/PostService.java
+++ src/main/java/com/lucasmd/workshopmongo/services/PostService.java
@@ -1,12 +1,12 @@
 package com.lucasmd.workshopmongo.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.lucasmd.workshopmongo.domain.Post;
-import com.lucasmd.workshopmongo.domain.User;
 import com.lucasmd.workshopmongo.repository.PostRepository;
 import com.lucasmd.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -20,6 +20,10 @@ public class PostService {
 		Optional<Post> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 		
+	}
+	
+	public List<Post> findByTitle(String text){
+		return repo.findByTitleContainingIgnoreCase(text);
 	}
 	
 }
